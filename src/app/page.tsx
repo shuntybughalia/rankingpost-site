dc47f9c3a8f90b20251ca13f112a20deb7d8fe5b
santokh <santokh@gmail.com>
2026-03-01 12:26:25 +0530

    fixing favicon and push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,22 @@ const Page = () => {
   return (
     <div>
       <Home/>
+      // src/app/page.tsx
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold text-indigo-600">
+        Welcome to RankingPost Site
+      </h1>
+      <p className="mt-4 text-gray-500">
+        Apni Raah edtech: Where dreams find a direction.
+      </p>
+      <button className="mt-6 px-6 py-2 bg-black text-white rounded-lg">
+        Explore Blogs
+      </button>
+    </main>
+  );
+}
     </div>
   );
 }
